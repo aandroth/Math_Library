1e33b4d99d1169a8171f651e28ccd94a4588fb26
aaron.andrews <aaron.andrews@SEA-8D0AB9>
2016-10-18 17:56:08 -0700

    Added jumping around.

--- Components/Main.cpp
+++ Components/Main.cpp
@@ -8,12 +8,13 @@
 #include <time.h>
 #include <iostream>
 #include "Mat3.h"
+#include <vector>
 
 
 int main()
 {
 	// Create the window
-	sfw::initContext(800, 600, "NSFW Draw");
+	sfw::initContext(1000, 1000, "NSFW Draw");
 	unsigned int font = sfw::loadTextureMap("./res/tonc_font.png", 16, 6);
 
 	float t = 0, spacer = 0;
@@ -23,23 +24,49 @@ int main()
 
 	float grav = 0;
 	int timeStep = 0;
-	Transform tra(200.0, 200.0, 1, 1, 0);
+	Transform tra(12 * 10 + 100, -8 * 10 + 100, 1, 1, 80);
 	RigidBody rig;
 	SpaceshipLocomotion space;
 	tra.m_position = Vec2(100, 100);
 	rig.velocity = Vec2(0, 0);
 	int maxHieght = tra.m_position.y + 3;
-	
-	Transform playerTransform(400, 300),
-		ST1(100, 0),
-		ST2(180, 0),
-		ST3(230, 0),
-		ST4(300, 0);
 
-	ST1.m_parent = &playerTransform;//
-	ST2.m_parent = &playerTransform;//&ST1;
-	ST3.m_parent = &playerTransform;//&ST2;
-	ST4.m_parent = &playerTransform;// &ST3;
+	// Tank Transform object
+	Transform tankTransform(12 * 10 + 100, -8 * 10 + 100, 1, 1, 80);
+
+	// Turret Transform object
+
+
+	std::vector<Vec2> wayPoints;
+	wayPoints.push_back(Vec2( 15 * 10 + 400,  18 * 10 + 100));
+	wayPoints.push_back(Vec2(  5 * 10 + 400,   8 * 10 + 100));
+	wayPoints.push_back(Vec2(-22 * 10 + 400,  -5 * 10 + 100));
+	wayPoints.push_back(Vec2(  4 * 10 + 400,  -2 * 10 + 100));
+	wayPoints.push_back(Vec2( -6 * 10 + 400,   9 * 10 + 100));
+	wayPoints.push_back(Vec2( 18 * 10 + 400,  88 * 10 + 100));
+	wayPoints.push_back(Vec2(-22 * 10 + 400, -90 * 10 + 100));
+
+	std::vector<Vec2> interestPoints;
+	interestPoints.push_back(Vec2(-4 * 10 + 400,    -4 * 10 + 100));
+	interestPoints.push_back(Vec2(21 * 10 + 400,    45 * 10 + 100));
+	interestPoints.push_back(Vec2( 7 * 10 + 400, -2148 * 10 + 100));
+	interestPoints.push_back(Vec2( 0 * 10 + 400,     0 * 10 + 100));
+	
+	float time = 0;
+	int lookingAt = 0;
+	float rotate = 0;
+	float move = 0;
+
+	//Transform playerTransform(400, 300),
+	//	ST1(100, 0),
+	//	ST2(180, 0),
+	//	ST3(230, 0),
+	//	ST4(300, 0);
+
+	//ST1.m_parent = &playerTransform;//
+	//ST2.m_parent = &playerTransform;//&ST1;
+	//ST3.m_parent = &playerTransform;//&ST2;
+	//ST4.m_parent = &playerTransform;// &ST3;
 
 	while (sfw::stepContext())
 	{
@@ -120,7 +147,6 @@ int main()
 		//	onV1 = !onV1;
 		//}
 
-
 		//t += 1;
 		//spacer += 0.5;
 
@@ -175,14 +201,14 @@ int main()
 		////std::cout << tra.position.y << std::endl;
 		//tra.position = tra.position + rig.velocity * sfw::getDeltaTime();
 
-		if (playerTransform.m_position.x <= -30)
-			playerTransform.m_position.x = 830;
-		else if (playerTransform.m_position.x > 830)
-			playerTransform.m_position.x = -30;
-		if (playerTransform.m_position.y > 630)
-			playerTransform.m_position.y = -30;
-		else if (playerTransform.m_position.y < -30)
-			playerTransform.m_position.y = 630;
+		//if (playerTransform.m_position.x <= -30)
+		//	playerTransform.m_position.x = 830;
+		//else if (playerTransform.m_position.x > 830)
+		//	playerTransform.m_position.x = -30;
+		//if (playerTransform.m_position.y > 630)
+		//	playerTransform.m_position.y = -30;
+		//else if (playerTransform.m_position.y < -30)
+		//	playerTransform.m_position.y = 630;
 
 		//ST1.m_facing = ST1.m_parent->m_facing + 20; //.setDirection(ST1.m_parent->getDirection() + Vec2(1.25, 1.25));
 		//ST2.m_facing = ST2.m_parent->m_facing + 40; //.setDirection(ST2.m_parent->getDirection() + Vec2(1.25, 1.25));
@@ -194,17 +220,61 @@ int main()
 		//tra.debugDraw();
 		//sfw::drawString(font, std::to_string(sfw::getDeltaTime()).c_str(), 400, 600, 48, 48, 0, ' ');
 
-		space.update(playerTransform, rig, sfw::getDeltaTime());
-		rig.integrate(playerTransform, sfw::getDeltaTime());
-		//tra.debugDraw();
-		playerTransform.debugDraw();
-		rig.debugDraw(playerTransform);
-		ST1.debugDraw();
-		ST2.debugDraw();
-		ST3.debugDraw();
-		ST4.debugDraw();
-	}
+		space.update(tankTransform, rig, sfw::getDeltaTime());
+		rig.integrate(tankTransform, sfw::getDeltaTime());
+		////tra.debugDraw();
+		//playerTransform.debugDraw();
+		//rig.debugDraw(playerTransform);
+		//ST1.debugDraw();
+		//ST2.debugDraw();
+		//ST3.debugDraw();
+		//ST4.debugDraw();
 
+
+		for (int ii = 0; ii < wayPoints.size(); ++ii)
+		{
+			if(ii == lookingAt)
+				sfw::drawCircle(wayPoints[ii].x, wayPoints[ii].y, 5, 12, RED);
+			else
+				sfw::drawCircle(wayPoints[ii].x, wayPoints[ii].y, 5, 12, GREEN);
+		}
+
+		for (int ii = 0; ii < interestPoints.size(); ++ii)
+		{
+			sfw::drawCircle(interestPoints[ii].x, interestPoints[ii].y, 5, 12, YELLOW);
+		}
+
+		//if (rotate == 0)
+		//{
+		//	rotate = angle(wayPoints[lookingAt] - tankTransform.m_position) - tankTransform.m_facing;
+		//	tankTransform.rotateLocalTransform(rotate);
+		//	std::cout << "rotate: " << rotate << std::endl;
+		//	system("pause");
+		//}
+		//else if (move == 0)
+		//{
+		//	std::cout << "move: " << move << std::endl;
+		//	rotate = 0;
+		//	move = 0;
+		//	++lookingAt;
+		//	system("pause");
+		//}
+
+		rotate = angle(wayPoints[lookingAt] - tankTransform.m_position) - tankTransform.m_facing;
+		tankTransform.rotateLocalTransform(rotate);
+		move = magnitude(wayPoints[lookingAt] - tankTransform.m_position);
+		tankTransform.translateLocalTransform(move);
+		++lookingAt;
+		system("pause");
+/*
+		time += sfw::getDeltaTime();
+		int cutTime = time;*/
+
+
+		tankTransform.debugDraw();
+
+
+	}
 	sfw::termContext();
 
 	return 0;
--- Components/Transform.cpp
+++ Components/Transform.cpp
@@ -24,13 +24,24 @@ Vec2 Transform::getUp() const
 	return perp(getDirection()) * -1;
 }
 
+void Transform::rotateLocalTransform(float const &R)
+{
+	//rotateByMat3 = R;
+	m_facing += R;
+}
+
+void Transform::translateLocalTransform(float const &T)
+{
+	moveBy = T;
+}
+
 Mat3 Transform::getLocalTransform() const
 {
-	Mat3 T = translate(m_position.x, m_position.y);
+	Mat3 T = translate(m_position.x, m_position.y) * translateByMat3;
 	Mat3 S = scale(m_scale.x, m_scale.y);
-	Mat3 R = rotateByDegrees(m_facing);
+	Mat3 R = rotateByDegrees(m_facing);// *rotateByMat3;
 
-	return T * R * S;
+	return  T * R * S;
 }
 
 Mat3 Transform::getSunTransform() const
@@ -51,18 +62,26 @@ Mat3 Transform::getGlobalTransform() const
 		//return m_parent->getGlobalTransform() * getLocalTransform();
 }
 
-void Transform::debugDraw(const Mat3 &T) const
+void Transform::debugDraw(const Mat3 &T)
 {
 	Mat3 L = T * getGlobalTransform();
 
 	Vec3 pos = Vec3(L.z1, L.z2, L.z3);
-
-	Vec3 right = L * Vec3(10, 0, 1);
+	Vec3 move = L * Vec3(moveBy, 0, 1);
+	Vec3 right = L * Vec3(20, 0, 1);
 	Vec3 up    = L * Vec3(0, 10, 1);
 
-	sfw::drawLine(pos.x, pos.y, up.x, up.y, RED);
-	sfw::drawLine(pos.x, pos.y, right.x, right.y, GREEN);
+	//sfw::drawLine(pos.x, pos.y, up.x, up.y, RED);
+	sfw::drawLine(m_position.x, m_position.y, right.x, right.y, GREEN);
 
+	pos = move;
+	m_position.x = pos.x;
+	m_position.y = pos.y;
 
-	sfw::drawCircle(pos.x, pos.y, 12, 12, WHITE);
+	//sfw::drawLine(pos.x,                    pos.y,                    right.x,                   right.y,				   GREEN);
+	//sfw::drawLine(right.x,                  right.y,                  pos.x + (right.x - up.x) , pos.y + (up.y - right.y), GREEN);
+	//sfw::drawLine(pos.x + (right.x - up.x), pos.y + (up.y - right.y), up.x,                      up.y,					   GREEN);
+	//sfw::drawLine(up.x,						up.y,					  pos.x,					 pos.y,					   GREEN);
+
+	sfw::drawCircle(pos.x, pos.y, 12, 12, GREEN);
 }
